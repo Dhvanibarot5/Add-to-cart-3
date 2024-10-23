318fd77df8163a7850a442ee4c31eb57b6031f03
Dhvanibarot5 <dhvanibarot5@gmail.com>
2024-10-23 21:55:26 +0530

    looping done

--- script.js
+++ script.js
@@ -1,79 +1,9 @@
-// const books = JSON.parse(localStorage.getItem("books")) || [];
-// const error = document.getElementById("error");
-// const bookTableBody = document.getElementById("bookTableBody");
-
-// const handleEdit = (index) => {
-//   console.log(index);
-// };
-// const handleDelete = (index) => {
-//   books.splice(index, 1);
-//   localStorage.setItem("books", JSON.stringify(books));
-//   PrintTable();
-// };
-
-// // console.log(books);
-
-// const PrintTable = () => {
-//   bookTableBody.innerHTML = "";
-
-//   books.map((book, index) => {
-//     const newRow = document.createElement("tr"); //<tr></tr>
-
-//     newRow.innerHTML = `
-//               <td class="py-2 px-4 border-b">${book.title}</td>
-//               <td class="py-2 px-4 border-b">${book.author}</td>
-//               <td class="py-2 px-4 border-b">${book.publisher}</td>
-//               <td class="py-2 px-4 border-b">${book.publishedDate}</td>
-//               <td class="py-2 px-4 border-b">${book.price}</td>
-//               <td class="py-2 px-4 border-b">
-//                 <button onClick="handleEdit(${index})">edit</button>
-//               </td>
-//               <td class="py-2 px-4 border-b">
-//                 <button onClick="handleDelete(${index})">delete</button>
-//               </td>`;
-//     bookTableBody.appendChild(newRow);
-//   });
-// };
-
-// PrintTable();
-
-// document.getElementById("btn").addEventListener("click", (e) => {
-//   e.preventDefault();
-//   const title = document.getElementById("title").value;
-//   const author = document.getElementById("author").value;
-//   const publisher = document.getElementById("publisher").value;
-//   const publishedDate = document.getElementById("published_date").value;
-//   const price = document.getElementById("price").value;
-
-//   if (!title || !author || !publisher || !publishedDate || !price) {
-//     error.innerHTML = "All fields are required.";
-//   } else {
-//     error.innerHTML = "";
-
-//     const newData = {
-//       title,
-//       author,
-//       publisher,
-//       publishedDate,
-//       price,
-//     };
-
-//     books.push(newData);
-
-//     localStorage.setItem("books", JSON.stringify(books));
-//     console.log(books);
-
-//     PrintTable();
-
-//     document.getElementById("title").value = "";
-//     document.getElementById("author").value = "";
-//     document.getElementById("publisher").value = "";
-//     document.getElementById("published_date").value = "";
-//     document.getElementById("price").value = "";
-//   }
-// });
-
 const products = [
+  {
+    image: "https://res.cloudinary.com/rixotech/image/upload/v1643262655/Test%20Project/watches_PNG9872_ywlrar.png",
+    name: "Exclusive Watch",
+    price: 999,
+  },
   {
     image: "images/2.jpg",
     name: "Watches",
@@ -101,22 +31,42 @@ const container = document.querySelector(".productContainer");
 const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 products.forEach((product, index) => {
-  const newProduct = ` <div class="w-3">
-            <div class="t_img1">
-              <div class="content">
-                <div class="teamCardImg">
-                  <img class="teamImg" src=${product.image} alt="" />
-                </div>
-                <h2>${product.name}</h2>
-                <p>Price $${product.price}</p> 
-                <i data-name="${product.name}"
-                  data-price="${product.price}"
-                  data-image="${product.image}"
-                   class="fa-solid fa-cart-shopping"></i>
-              </div>
-            </div>
-          </div>`;
+  // const newProduct = ` <div class="w-3">
+  //           <div class="t_img1">
+  //             <div class="content">
+  //               <div class="teamCardImg">
+  //                 <img class="teamImg" src=${product.image} alt="" />
+  //               </div>
+  //               <h2>${product.name}</h2>
+  //               <p>Price $${product.price}</p>
+  //               <i data-name="${product.name}"
+  //                 data-price="${product.price}"
+  //                 data-image="${product.image}"
+  //                  class="fa-solid fa-cart-shopping"></i>
+  //             </div>
+  //           </div>
+  //         </div>`;
 
+  const newProduct = `<div class="card">
+          <div class="img">
+            <img src="https://res.cloudinary.com/rixotech/image/upload/v1643262655/Test%20Project/watches_PNG9872_ywlrar.png" alt="Watch" />
+          </div>
+          <div class="text">
+            <h3>Exclusive Watch</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veniam sunt numquam cumque hic!</p>
+            <h5>$ 125</h5>
+            <div class="rating">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+          </div>
+          <div class="btn">
+            <button>Add To Cart</button>
+          </div>
+        </div>`;
   container.innerHTML += newProduct;
 });
 
